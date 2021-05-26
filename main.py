445e5aceb140e2ac07414f231a91401135129a1e
Abhijay Wadhwani <Abhijay-W@users.noreply.github.com>
2021-05-26 23:18:58 +0530

    working api and pages

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 from fastapi import FastAPI, File, UploadFile
 from fastapi.staticfiles import StaticFiles
-import cv2
-from fastapi.responses import HTMLResponse, StreamingResponse
+import shutil
+import os
+from fastapi.responses import HTMLResponse
 from fer import FER
 import matplotlib.pyplot as plt
 import aiofiles
+
 app = FastAPI()
 
 app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 @app.get('/', response_class=HTMLResponse)  # basic get view
 async def getRoot():
@@ -15,9 +18,36 @@ async def getRoot():
         data = await f.read()
     return data
 
-@app.post('/predict')
-async def predict(file: UploadFile = File(...)):
-    img = plt.imread(file.filename)
+
+@app.get('/detect', response_class=HTMLResponse)
+async def proceed():
+    async with aiofiles.open("templates\emotions.html", mode="r") as f:
+        data = await f.read()
+    return data
+
+
+@app.post("/upload")
+def emotiondetection(image: UploadFile = File(...)):
+    save_file(image, path="temp", save_as="temp")
+    return{"text": "File Uploaded Successfully"}
+
+
+@app.post("/predict")
+def emotions():
+    img = plt.imread("temp/temp.jpg")
     detector = FER(mtcnn=True)
-    emo = detector.detect_emotions(img)[0]['emotions']
-    return {"Emotions Detected": emo}
+    key_value = detector.detect_emotions(img)[0]['emotions']
+    emo = {}
+    sorted_keys = sorted(key_value, key=key_value.get, reverse=True)
+    for w in sorted_keys:
+        emo[w] = key_value[w]
+    emotions = list(emo.keys())[0:3]
+    return {"emo": emotions}
+
+
+def save_file(uploaded_file, path=".", save_as="default"):
+    extension = os.path.splitext(uploaded_file.filename)[-1]
+    temp_file = os.path.join(path, save_as + extension)
+    with open(temp_file, "wb") as buffer:
+        shutil.copyfileobj(uploaded_file.file, buffer)
+    return temp_file
